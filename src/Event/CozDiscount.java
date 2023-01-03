b2023ba32f45516f3985779ec3dbd35152298636
serveman <120380939+serveman@users.noreply.github.com>
2023-01-03 14:07:03 +0900

    Delete src directory

--- src/Event/CozDiscount.java
+++
@@ -1,9 +0,0 @@
-package Event;
-
-public class CozDiscount implements Discount {
-    int discountRate = 10;
-
-    public CozDiscount(int discountRate) {
-        this.discountRate = discountRate;
-    }
-}
--- src/Event/Youth.java
+++
@@ -1,9 +0,0 @@
-package Event;
-
-public class Youth implements Discount {
-    int discountAmount = 500;
-
-    public Youth(int discountAmount) {
-        this.discountAmount = discountAmount;
-    }
-}
